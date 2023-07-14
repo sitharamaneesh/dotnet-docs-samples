84111cc808c89ce0e06336aa43cb286979962c48
SITHARA MANEESH <91305911+sitharamaneesh@users.noreply.github.com>
2023-07-14 20:07:54 +0530

    Update Program.cs

--- aspnetapp/Program.cs
+++ aspnetapp/Program.cs
@@ -1,23 +1,79 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+app.MapHealthChecks("/healthz");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+// ...
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.ConfigureKestrel(options =>
-                {
-                    options.ListenAnyIP(80); // Listen on port 80 for HTTP
-                })
-                .UseStartup<Startup>();
-            });
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.Use((context, next) =>
+    {
+        if (context.Request.Scheme != "https")
+        {
+            var httpsUrl = $"https://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}";
+            context.Response.Redirect(httpsUrl);
+            return Task.CompletedTask;
+        }
+
+        return next();
+    });
 }
 
+// ...
 
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+CancellationTokenSource cancellation = new();
+app.Lifetime.ApplicationStopping.Register( () =>
+{
+    cancellation.Cancel();
+});
+
+app.MapGet("/Environment", () =>
+{
+    return new EnvironmentInfo();
+});
+
+// This API demonstrates how to use task cancellation
+// to support graceful container shutdown via SIGTERM.
+// The method itself is an example and not useful.
+app.MapGet("/Delay/{value}", async (int value) =>
+{
+    try
+    {
+        await Task.Delay(value, cancellation.Token);
+    }
+    catch(TaskCanceledException)
+    {
+    }
+    
+    return new {Delay = value};
+});
+
+app.Run();
