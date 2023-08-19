955aae78d4a06dd644bd17392981d73e03fa9dcc
SITHARA MANEESH <91305911+sitharamaneesh@users.noreply.github.com>
2023-08-19 19:27:10 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ app.MapGet("/", async context =>
     string osName = Environment.OSVersion.Platform.ToString();
     string osVersion = Environment.OSVersion.Version.ToString();
     var responseMessage = $"<div style='text-align:center;'>" +
-                          $"<h1>Hello, welcome to dotnet app!</h1>" +
+                          $"<h1>Hello, welcome to dotnet app!++++++++</h1>" +
                           $"<p>ASP.NET Version: {aspnetVersion}</p>" +
                           $"<p>Server Time: {serverTime}</p>" +
                           $"<p>Os details: {osName} {osVersion}</p>" +
